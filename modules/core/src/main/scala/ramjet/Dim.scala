5da9167ab4f7b05f1873438c2e6e98695429a506
vbergeron <valentin.bergeron@ledger.fr>
2024-02-19 11:21:31 +0100

    Better use of litteral types

--- modules/core/src/main/scala/ramjet/Dim.scala
+++ modules/core/src/main/scala/ramjet/Dim.scala
@@ -9,9 +9,7 @@ import compiletime.ops.any.ToString as Str
 object Dim {
 
   inline def apply[I <: Int, J <: Int]: (I, J) =
-    inline val I = constValue[I]
-    inline val J = constValue[J]
-    (I, J)
+    constValueTuple[(I, J)]
 
   // helper to format dimentions to a readable string
   inline def fmt[I <: Int]: "[" + Str[I] + "]" = constValue
@@ -27,46 +25,34 @@ object Dim {
 
   // Checks if we can reshape an 2d tensor into another one
   inline def checkT2toT2[N <: Int, M <: Int, P <: Int, Q <: Int]: Boolean =
-    inline if elems[N, M] == elems[P, Q] then true
+    inline if constValue[N * M == P * Q] then true
     else error("Error reshaping " + fmt[N, M] + " to " + fmt[P, Q])
 
   // Checks if we can reshape an 1d tensor into a 2d one
   inline def checkT1toT2[N <: Int, P <: Int, Q <: Int]: Boolean =
-    inline if constValue[N] == elems[P, Q] then true
+    inline if constValue[N == P * Q] then true
     else error("Error reshaping " + fmt[N] + " to " + fmt[P, Q])
 
   // Checks if we can reshape an 1d tensor into a 2d one
   inline def checkT2toT1[N <: Int, M <: Int, Q <: Int]: Boolean =
-    inline if elems[N, M] == constValue[Q] then true
+    inline if constValue[N * M == Q] then true
     else error("Error reshaping " + fmt[N, M] + " to " + fmt[Q])
 
   // Checks if 2 2d tensors can be multiplied together
   inline def checkT2xT2[N <: Int, M <: Int, P <: Int, Q <: Int]: Boolean =
-    inline val M: Int = constValue[M]
-    inline val P: Int = constValue[P]
-
-    inline if M == P then true
+    inline if constValue[M == P] then true
     else error("Wrong tensor format: " + fmt[N, M] + " x " + fmt[P, Q])
 
   inline def checkT1xT1[N <: Int, Q <: Int]: Boolean =
-    inline val N: Int = constValue[N]
-    inline val Q: Int = constValue[Q]
-
-    inline if N == Q then true
+    inline if constValue[N == Q] then true
     else error("Wrong tensor format: " + fmt[N] + " x " + fmt[Q])
 
   inline def checkT1xT2[N <: Int, P <: Int, Q <: Int]: Boolean =
-    inline val N: Int = constValue[N]
-    inline val P: Int = constValue[P]
-
-    inline if N == P then true
+    inline if constValue[N == P] then true
     else error("Wrong tensor format: " + fmt[N] + " x " + fmt[P, Q])
 
   // Checks if 2 2d tensors can be multiplied together
   inline def checkT2xT1[N <: Int, M <: Int, Q <: Int]: Boolean =
-    inline val M: Int = constValue[M]
-    inline val Q: Int = constValue[Q]
-
-    inline if M == Q then true
+    inline if constValue[M == Q] then true
     else error("Wrong tensor format: " + fmt[N, M] + " x " + fmt[Q])
 }
